38923ddd805910c08528216a7d6c37d015bc619a
Sammy Wu <swucode@gmail.com>
2018-02-02 06:17:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function BoardMember(name, homeState, training) {
   this.veto = function() {
     console.log("No, I must disagree");
   }
+  this.approve = function() {
+    console.log("You can do that!")
+  }
+  this.doCharity = function () {
+    console.log("I like to help people.")
+  }
 }
